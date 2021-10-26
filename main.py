7f6f7eec1f28d7e84588ff3aab5efabcb91aefff
Lesson <2010913@brunel.ac.uk>
2021-10-26 14:54:05 +0100

    first commit

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-list = ["Ben", 9, [2, 1]]
-print(list)
-list.append("Lorenzo")
-print(list)
-del list[0]
-lorenzo = list.pop(2)
-print(lorenzo)
-print(list)
-numbered_list = [89, 56, 99, 2]
-print(sorted(numbered_list, reverse=True))
+# list = ["Ben", 9, [2, 1]]
+# print(list)
+# list.append("Lorenzo")
+# print(list)
+# del list[0]
+# lorenzo = list.pop(2)
+# print(lorenzo)
+# print(list)
+# numbered_list = [89, 56, 99, 2]
+# print(sorted(numbered_list, reverse=True))
+#
+#
+# inventory = ["tentacle sweeper", "sword"]
+# item = input("Which item will you use: ")
+#
+#
+# def checking_items(inventory2, item2):
+#     if item2 in inventory2 and item2 == "tentacle sweeper":
+#         print("yes")
+#     else:
+#         print("no")
+#
+#
+# checking_items(inventory, item)
+#
+# list = [1, 6, 3]
+# for item in list:
+#     print(item)
+#
+# for item in range(5, 10, 2):
+#     print(item, end=",")
 
-
-inventory = ["tentacle sweeper", "sword"]
-item = input("Which item will you use: ")
-
-
-def checking_items(inventory2, item2):
-    if item2 in inventory2 and item2 == "tentacle sweeper":
-        print("yes")
-    else:
-        print("no")
-
-
-checking_items(inventory, item)
+string = "python"
+for item in string:
+    if item == "t":
+        continue
+    print(item, end="")
 
 
